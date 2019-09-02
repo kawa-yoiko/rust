a17b3202d73f40840dd2fa70593d1a3bd25b945e
Shiqing <shiqing-thu18@yandex.com>
2019-09-02 21:03:35 +0800

    Improve searching in rustdoc and add tests

---
+++ src/test/rustdoc-js/module-substring.js
@@ -0,0 +1,11 @@
+const QUERY = 'ig::pc';
+
+const EXPECTED = {
+    'others': [
+        { 'path': 'module_substring::Sig', 'name': 'pc' },
+        { 'path': 'module_substring::Si', 'name': 'pc' },
+        { 'path': 'module_substring::Foo', 'name': 'pc' },
+        { 'path': 'module_substring::Sig', 'name': 'pa' },
+        { 'path': 'module_substring::Si', 'name': 'pa' },
+    ],
+};
