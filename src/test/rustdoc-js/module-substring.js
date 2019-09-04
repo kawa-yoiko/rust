f5b3d5ca5cac8ebf1b45d506b494a517606b632e
Shiqing <shiqing-thu18@yandex.com>
2019-09-04 16:39:11 +0800

    Remove explicit checks of word equivalence priority
    Only ensure that t1::n1 -> t2::n1 -> t2::n2 are ordered

--- src/test/rustdoc-js/module-substring.js
+++ src/test/rustdoc-js/module-substring.js
@@ -4,8 +4,6 @@ const EXPECTED = {
     'others': [
         { 'path': 'module_substring::Sig', 'name': 'pc' },
         { 'path': 'module_substring::Si', 'name': 'pc' },
-        { 'path': 'module_substring::Foo', 'name': 'pc' },
-        { 'path': 'module_substring::Sig', 'name': 'pa' },
         { 'path': 'module_substring::Si', 'name': 'pa' },
     ],
 };
